d73f4b1d564bcafb1ff38e4387664c21f2031f3c
KevinKennyy <146836341+KevinKennyy@users.noreply.github.com>
2024-02-12 16:06:22 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -1,8 +1,20 @@
-from utils import db_connect
-engine = db_connect()
+import tkinter as tk
+from tkinter import messagebox
 
-# your code here
-import pandas as pd
+def mostrar_mensaje():
+    messagebox.showinfo("Mensaje", "DORADITA")
 
-total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
-total_data.head()
+# Crear la ventana principal
+root = tk.Tk()
+root.title("Ventana con botón")
+
+# Función para mostrar el mensaje
+def mostrar_mensaje():
+    messagebox.showinfo("Mensaje", "DORADITA")
+
+# Crear el botón
+boton = tk.Button(root, text="Lo siento", command=mostrar_mensaje)
+boton.pack(pady=20)
+
+# Ejecutar el bucle principal
+root.mainloop()
